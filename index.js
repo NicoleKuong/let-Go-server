1d8ec04ed619c8ea04d708722e74bf780e2d0f20
NicoleKuong <nicolekuong@gmail.com>
2020-03-15 18:24:44 +0100

    add post request for payment router

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const userRouter = require("./user/router");
 const authRouter = require("./auth/router");
 const itemRouter = require("./item/router");
 const emailRouter = require("./email/router");
+const paymentRouter = require("./payment/router");
 
 const app = express();
 
@@ -18,6 +19,7 @@ app.use(userRouter);
 app.use(authRouter);
 app.use(itemRouter);
 app.use(emailRouter);
+app.use(paymentRouter);
 
 app.get("/", (request, response, next) => {
   response.send("hello world");
--- payment/router.js
+++ payment/router.js
@@ -1,3 +1,56 @@
 const { Router } = require("express");
-const stripe = require("stripe");
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
+const stripePublicKey = process.env.STRIPE_PUBLIC_KEY;
+const stripe = require("stripe")(stripeSecretKey);
 const uuid = require("uuid/v4");
+
+const router = new Router();
+
+router.post("/payment", async (request, response, next) => {
+  console.log("payment", request.body);
+  const { item, token } = request.body;
+  console.log("product++", item);
+  console.log("price ---", item.price);
+  const idempontencyKey = uuid();
+  try {
+    // Create a new customer and then create an invoice item then invoice it:
+    const newCustomers = await stripe.customers
+      .create({
+        email: token.email,
+        source: token.id
+      })
+      .then(customer => {
+        stripe.charges.create(
+          {
+            amount: item.price * 100,
+            currency: "eur",
+            customer: customer.id,
+            receipt_email: token.email,
+            description: `Rented the ${item.name}`,
+            shipping: {
+              name: token.card.name,
+              address: {
+                line1: token.card.address_link1,
+                city: token.card.address_city,
+                country: token.card.address_country,
+                postal_code: token.card.address_zip
+              }
+            }
+          },
+          { idempontencyKey }
+        );
+      })
+      .then(() => {
+        console.log("Charge Successful");
+        response.status(200).json({ message: "Successfully rented item" });
+      });
+  } catch (error) {
+    console.log("Charge fail");
+    next(error);
+  }
+});
+
+module.exports = router;
