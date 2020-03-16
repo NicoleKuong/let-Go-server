121ec25807159c3480b4ea2cb631b2f90b636a35
NicoleKuong <nicolekuong@gmail.com>
2020-03-16 02:33:18 +0100

    modifed payment router and successfully made payment with stripe

--- payment/router.js
+++ payment/router.js
@@ -3,54 +3,51 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
-const stripePublicKey = process.env.STRIPE_PUBLIC_KEY;
 const stripe = require("stripe")(stripeSecretKey);
-const uuid = require("uuid/v4");
+const { v4: uuidv4 } = require("uuid");
 
 const router = new Router();
 
 router.post("/payment", async (request, response, next) => {
-  console.log("payment", request.body);
-  const { item, token } = request.body;
-  console.log("product++", item);
-  console.log("price ---", item.price);
-  const idempontencyKey = uuid();
+  console.log("product++", request.body);
+  console.log("price ---", request.body.price);
+
+  let error;
+  let status;
   try {
+    const { token, title, price } = request.body;
     // Create a new customer and then create an invoice item then invoice it:
-    const newCustomers = await stripe.customers
-      .create({
-        email: token.email,
-        source: token.id
-      })
-      .then(customer => {
-        stripe.charges.create(
-          {
-            amount: item.price * 100,
-            currency: "eur",
-            customer: customer.id,
-            receipt_email: token.email,
-            description: `Rented the ${item.name}`,
-            shipping: {
-              name: token.card.name,
-              address: {
-                line1: token.card.address_link1,
-                city: token.card.address_city,
-                country: token.card.address_country,
-                postal_code: token.card.address_zip
-              }
-            }
-          },
-          { idempontencyKey }
-        );
-      })
-      .then(() => {
-        console.log("Charge Successful");
-        response.status(200).json({ message: "Successfully rented item" });
-      });
+    const customer = await stripe.customers.create({
+      email: token.email,
+      source: token.id
+    });
+    const idempotencyKey = uuidv4();
+    const charge = await stripe.charges.create(
+      {
+        amount: price * 100,
+        currency: "eur",
+        customer: customer.id,
+        receipt_email: token.email,
+        description: `Rented the ${title}`,
+        shipping: {
+          name: token.card.name,
+          address: {
+            line1: token.card.address_line1,
+            city: token.card.address_city,
+            country: token.card.address_country,
+            postal_code: token.card.address_zip
+          }
+        }
+      },
+      { idempotencyKey }
+    );
+    console.log("Charge Successful", { charge });
+    status = "success";
   } catch (error) {
-    console.log("Charge fail");
-    next(error);
+    console.error("Charge fail", error);
+    status = "failure";
   }
+  response.json({ error, status });
 });
 
 module.exports = router;
