d0bcde6a6ecfcecdc77f1e44c1bc946e8a16454c
NicoleKuong <nicolekuong@gmail.com>
2020-06-10 18:53:36 +0200

    minor fix

--- db.js
+++ db.js
@@ -1,13 +1,13 @@
 const Sequelize = require("sequelize");
 
 const dataBaseURL =
-  process.env.DATABASE_URL ||
+  // process.env.DATABASE_URL ||
   "postgres://postgres:secret@localhost:5432/postgres";
 
 console.log("process.env.DATABASE_URL", dataBaseURL);
 const db = new Sequelize(dataBaseURL);
 
-db.sync({ force: true })
+db.sync({ force: false })
   .then(() => console.log("Database connected"))
   .catch(console.error);
 
